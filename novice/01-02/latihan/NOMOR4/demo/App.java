356e6020bda452144d3fb29719ae8717aeae84bb
nasutioncode <nasutioncode@gmail.com>
2019-12-10 11:07:35 +0700

    h2

---
+++ novice/01-02/latihan/NOMOR4/demo/App.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class App {
+    public String getGreeting() {
+        return "Nasutiontion";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+
+ }
